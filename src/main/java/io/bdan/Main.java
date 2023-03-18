5081a27707061efbfaaaa068e78097941a96eb54
bdan <mail@bdan.io>
2023-03-18 13:51:09 +0100

    this is a spambot now :trollface:

--- src/main/java/io/bdan/Main.java
+++ src/main/java/io/bdan/Main.java
@@ -1,5 +1,7 @@
 package io.bdan;
 
+// amogus
+
 public class Main {
     public static void main(String[] args) {
 
--- src/main/java/io/bdan/scraper.java
+++ src/main/java/io/bdan/scraper.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.util.concurrent.TimeUnit;
+
 public class scraper {
 
     public static void scrape() {
@@ -15,8 +17,20 @@ public class scraper {
                 final String venue = row.select("div.showVenue").text();
                 final String location = row.select("div.showLocation").text();
                 final String availability = row.select("div.showTickets").text();
-                final String URL = row.select("a.ctaButton").attr("href");
-                System.out.println(date + " | " + venue + " | " + location + " | " +  availability +  " | " + URL);
+                String URL = row.select("a.ctaButton").attr("href");
+
+                discord discord = new discord();
+                discord.addEmbed(new discord.EmbedObject()
+                        .setTitle(location)
+                        .setDescription(venue + " — " + date + "\\n\\n["+ availability.toUpperCase() + "]("+ URL +")")
+                        .setUrl("https://deadmau5.com/shows"));
+                discord.execute();
+
+                if(URL == "")
+                    URL = "N/A";
+                System.out.println(date + " | " + venue + " | " + location + " | " +  availability.toUpperCase() +  " | " + URL);
+                TimeUnit.MILLISECONDS.sleep(100);
+
             }
 
             System.out.println("\n" + java.time.LocalDateTime.now() + "  |  Scraping complete...");
